319fbc53524110a5fcd5cc4e48b21785c916e567
Dodô Evandro <evandro.carreira@gmail.com>
2025-02-16 20:04:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="almost-full-page">
+        <h1>dod0</h1>
+        <div className="contact-footer">
+          <a href="mailto:evandro.carreira@gmail.com">
+            <p>evandro.carreira@gmail.com</p>
+          </a>
+          <a href="https://github.com/dodocinho">
+            <p>github.com/dodocinho</p>
+          </a>
+        </div>
+      </div>
+      <div className="projects">
+        <div>shaderslab</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
